3c2c04adcafe15666872c3a94198dc259423fc57
ibrahimabdullah570 <iabdullah@myseneca.ca>
2024-02-28 08:11:28 -0500

    localhost

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello WEB322!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
